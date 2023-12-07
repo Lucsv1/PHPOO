a320a911c4edbbae0c21b0b3ecc7190c9f129580
Lucas Vinicius Oliveira Galindo <lucas.galindo@corp.directweb.br>
2023-12-07 10:27:02 -0300

    Realizando meu primeiro metodo em php

--- phpteste.php
+++ phpteste.php
@@ -5,12 +5,18 @@ require_once "src/Conta.php";
 $conta = new Conta ();
 $conta -> cpfTitular = "12323123123";
 $conta -> nomeTitular = "Lucas Vincius";
+$conta -> saldo = 100;
 
 $conta1 = new Conta ();
 $conta1 -> saldo = 300;
 $conta1 -> cpfTitular = "3333333";
 $conta1 -> nomeTitular = "JoaO";
 
-$conta2 = $conta;
+
+
+
+$conta->depositar(200);
+$conta1->depositar(300);
 
 var_dump($conta);
+var_dump($conta1);
--- src/Conta.php
+++ src/Conta.php
@@ -1,15 +1,29 @@
-<?php 
+<?php
 
-class Conta {
+class Conta
+{
 
    public string $cpfTitular;
    public string $nomeTitular;
    public float $saldo;
 
 
-   public function sacar(float $valorSacar): Conta {
-    if ($valorSacar > $saldo) {
-        echo"valor acima";
+   public function sacar(float $valorSacar)
+   {
+      if ($valorSacar > $this->saldo) {
+         echo "valor acima";
+      } else {
+         $this->saldo -= $valorSacar;
+      }
+
+   }
+
+   public function depositar(float $valorDepositar){
+      if ($valorDepositar < 0){
+         echo "Naop tem como depositar valor negativo";
+      } else {
+         $this -> saldo += $valorDepositar;
+      }
    }
 
 }
